50f28db9b1d7de66d595fed40965a6f807db44df
SergiuRomaniuc <sergiu.romaniuc01@gmail.com>
2024-06-19 16:33:22 +0300

    s

--- proiect_licenta.py
+++ proiect_licenta.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from openai import OpenAI
 import os
 import dotenv
@@ -9,32 +10,33 @@ dotenv.load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")) #improting openai api key from env vars
 
 completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    response_format={ "type": "json_object" },
+    model="gpt-4o",
+    # response_format={ "type": "json_object" },
     messages=[
         {
             "role": "system",
-            "content": "You are a helpful assistant. You output JSON.",
+            "content": "You are a helpful assistant in the form of an intelligent speaker. You do not show text as response, but rather speak with the user.",
         },
         {
             "role": "user",
-            "content": "Poti sa imi spui despre tine, cine esti ?",
+            "content": "Salut asistentule, spune-mi despre tine, cine esti si cum functionezi, si povesteste-mi o istoriara interesanta.",
         },
     ],
+    # max_tokens=300,
 )
 
-raspuns = json.loads(completion.choices[0].message.content)
-for key in raspuns:
-    print(type(raspuns[key]))
-print(completion.choices[0].message)
+raspuns = completion.choices[0].message.content
+# for key in raspuns:
+#     print(type(raspuns[key]))
+# print(completion.choices[0].message)
+print(raspuns)
 
-
-
+file_path = Path(__file__).parent / "output.mp3"
 response = client.audio.speech.create(
     model="tts-1",
     voice="echo",
-    input=raspuns[list(raspuns)[0]],
+    input=raspuns
 )
 
-response.stream_to_file("output.mp3")
+response.stream_to_file(file_path)
 # response.with_streaming_response.methond()
