b88bf37485970c28538e9985caacc37aeebbe36c
SergiuRomaniuc <sergiu.romaniuc01@gmail.com>
2024-06-25 14:49:06 +0300

    Fiecare parte a procesului de conversatie implementata

--- proiect_licenta.py
+++ proiect_licenta.py
@@ -2,41 +2,61 @@ from pathlib import Path
 from openai import OpenAI
 import os
 import dotenv
-import json
+# import json
+from playsound import playsound
+from devices import record
 
 dotenv.load_dotenv()
 
 
+context = "limba vorbita: romana"
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")) #improting openai api key from env vars
+while True:
+    record()
+    input_audio_file=open("input.wav", "rb")
+    transcription = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=input_audio_file
+    language="romanian"
+    )
 
-completion = client.chat.completions.create(
-    model="gpt-4o",
-    # response_format={ "type": "json_object" },
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a helpful assistant in the form of an intelligent speaker. You do not show text as response, but rather speak with the user.",
-        },
-        {
-            "role": "user",
-            "content": "Salut asistentule, spune-mi despre tine, cine esti si cum functionezi, si povesteste-mi o istoriara interesanta.",
-        },
-    ],
-    # max_tokens=300,
-)
+    print(transcription.text)
 
-raspuns = completion.choices[0].message.content
-# for key in raspuns:
-#     print(type(raspuns[key]))
-# print(completion.choices[0].message)
-print(raspuns)
+    
 
-file_path = Path(__file__).parent / "output.mp3"
-response = client.audio.speech.create(
-    model="tts-1",
-    voice="echo",
-    input=raspuns
-)
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        # response_format={ "type": "json_object" },
+        messages=[
+            {
+                "role": "system",
+                "content": context,
+            },
+            {
+                "role": "user",
+                "content": transcription.text,
+            },
+        ],
+        # max_tokens=300,
+    )
 
-response.stream_to_file(file_path)
-# response.with_streaming_response.methond()
+    raspuns = completion.choices[0].message.content
+    # for key in raspuns:
+    #     print(type(raspuns[key]))
+    # print(completion.choices[0].message)
+    print(raspuns)
+
+    context = context + "\nuser question: " + transcription.text + "\nchatgpt response: " + raspuns 
+    
+    file_path = Path(__file__).parent / "output.mp3"
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="echo",
+        input=raspuns
+    )
+
+    response.stream_to_file(file_path)
+    # response.with_streaming_response.methond()
+
+    playsound('C:/Users/roman/VS_Code/Licenta/output.mp3')
